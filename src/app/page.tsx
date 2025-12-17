79ee3e36eb27ba5b71e49c2c55d47a95120b67b3
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-12-17 09:36:04 -0500

    latest commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignOutButton,
-  SignUpButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <>
-      <div className="">
-        <SignedOut>
-          <SignUpButton />
-        </SignedOut>
-        <SignedIn>
-          <SignOutButton />
-        </SignedIn>
-      </div>
+      <div className="">Home </div>
     </>
   );
 }
