5885c760216c451f002971b666d2bcbc830a0c83
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2025-12-20 22:23:50 -0500

    latest commit

---
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditorPanel() {
+  return <div>EditorPanel</div>;
+}
+
+export default EditorPanel;
---
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OutputPanel() {
+  return <div>OutputPanel</div>;
+}
+
+export default OutputPanel;
